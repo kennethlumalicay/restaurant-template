e3e05dfca50afafb4e4338918614c261bce2afe4
kennethlumalicay <kennethlumalicay@gmail.com>
2018-03-07 19:00:40 -0500

    fixed onblur

--- src/Nav.js
+++ src/Nav.js
@@ -14,13 +14,16 @@ class Nav extends Component {
       pastLanding: false
     }
   }
+
   componentDidMount() {
     document.addEventListener('scroll', e => this.handleScroll(e));
     this.pageChange(this.props);
   }
+
   componentWillReceiveProps(next) {
     this.pageChange(next);
   }
+
   pageChange(props) {
     if(!props.match.params.page) {
       this.setState({
@@ -32,6 +35,7 @@ class Nav extends Component {
       });
     }
   }
+
   handleScroll(e) {
     const { pastLanding, hideNav } = this.state;
     const { scrollY, innerHeight } = window;
@@ -48,6 +52,7 @@ class Nav extends Component {
       });
     }
   }
+
   toggleNav(e) {
     const { navBg, pastLanding, hideNav } = this.state;
     this.setState({
@@ -55,33 +60,38 @@ class Nav extends Component {
       navBg: pastLanding ? true : !navBg
     });
   }
-  handleBlur(e) {
+
+  handleBlur(event) {
+    const { currentTarget } = event;
     setTimeout(() => {
-      this.setState({
-        hideNav: true,
-        navBg: this.state.pastLanding
-      });
-    },150);
+      if(!currentTarget.contains(document.activeElement)) {
+        this.collapseNav();
+      }
+    });
   }
+
+  collapseNav() {
+    this.setState({
+      hideNav: true,
+      navBg: this.state.pastLanding
+    })
+  }
+
   render() {
     const { hideNav, navBg } = this.state;
     const navLinksClass = 'navlinks' + (hideNav ? ' hide-nav' : '') + (navBg ? ' nav-bg' : '');
     const navClass = 'nav' + (navBg ? ' nav-bg' : '');
 
     return (
-      <nav className={navClass}>
+      <nav className={navClass} tabIndex='1' onBlur={e => hideNav ? null : this.handleBlur(e)}>
         <div className='company'>
           <NavLink exact to='/'>
             <img src={logo} className='logo' alt='logo'/>
             <span>Lens Vision</span>
           </NavLink>
         </div>
-        <div className='navbtn'
-          tabIndex='0'
-          onClick={(e) => this.toggleNav(e)}
-          onBlur={(e) => this.handleBlur(e)}
-        >Menu</div>
-        <div className={navLinksClass}>
+        <div className='navbtn' onClick={(e) => this.toggleNav(e)}>Menu</div>
+        <div className={navLinksClass} onClick={e => this.collapseNav()}>
           <NavLink exact to='/' activeClassName={this.active}>Home</NavLink>
           <NavLink to='/about' activeClassName={this.active}>About</NavLink>
           <NavLink to='/services' activeClassName={this.active}>Services</NavLink>
